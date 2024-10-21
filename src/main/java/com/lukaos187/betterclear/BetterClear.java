87bc7b266f38954552fdb818067f46275e35a9e7
Lukaos187 <lukas.jensen@web.de>
2024-10-21 15:02:32 +0200

    ADD the final touches for the release.

--- src/main/java/com/lukaos187/betterclear/BetterClear.java
+++ src/main/java/com/lukaos187/betterclear/BetterClear.java
@@ -3,16 +3,17 @@ package com.lukaos187.betterclear;
 import com.lukaos187.betterclear.commands.Clear;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
+/**BetterClear is a Minecraft plugin that enhances the vanilla /clear command, allowing administrators to clear players'
+ *  inventories with precision by category (e.g., blocks, tools, armor) and specific areas
+ *  (hotbar, inventory, armor slots). A documentation of this plugin can be found
+ <a href= "https://github.com/AwayAllay/better-clear">here</a>.*/
 public class BetterClear extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getCommand("betterClear").setExecutor(new Clear());
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        Objects.requireNonNull(getCommand("betterClear")).setExecutor(new Clear());
     }
 }
