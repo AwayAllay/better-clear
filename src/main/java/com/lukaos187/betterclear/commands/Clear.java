2cba815ea4d48d7ae9a77ca8abd74a1a60277bc2
Lukaos187 <lukas.jensen@web.de>
2024-10-21 15:02:32 +0200

    ADD the final touches for the release.

--- src/main/java/com/lukaos187/betterclear/commands/Clear.java
+++ src/main/java/com/lukaos187/betterclear/commands/Clear.java
@@ -14,6 +14,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+/**The command for clearing the players´ inventory in a better way. Can be called with /betterclear.
+ *  The correct order to call this command is: /betterclear name category section.*/
 public class Clear implements TabExecutor {
 
     private final List<String> category = Arrays.asList("blocks","armor","tools","items","*");
@@ -88,19 +90,24 @@ public class Clear implements TabExecutor {
     private boolean checkForNull(String[] args, Player player) {
 
         if (args.length == 0 || args[0] == null || Bukkit.getPlayer(args[0]) == null){
+            if (args.length == 0 || (args[0] != null && !args[0].equalsIgnoreCase("*"))){
 
-            if (args[0] != null && !args[0].equalsIgnoreCase("*")){
-
-                player.sendMessage(ChatColor.GRAY + "Please provide a valid player to clear.");
+                player.sendMessage(ChatColor.GRAY + "Please provide a valid" + ChatColor.WHITE + " player" + ChatColor.GRAY + " to clear.");
+                player.sendMessage(ChatColor.WHITE + "---------------------------------");
+                player.sendMessage(ChatColor.GRAY + "A precise description on how to use this plugin can be found here: "
+                        + ChatColor.WHITE + "https://github.com/AwayAllay/better-clear#better-clear");
+                player.sendMessage(ChatColor.WHITE + "---------------------------------");
                 return false;
             }
         }
-        if (args[1] == null || !category.contains(args[1])){
-            player.sendMessage(ChatColor.GRAY + "Please provide a valid itemType to clear.");
+        if (args.length == 1 || (args[1] == null || !category.contains(args[1]))){
+            player.sendMessage(ChatColor.GRAY + "Please provide a valid" + ChatColor.WHITE + " category" + ChatColor.GRAY + " to clear.");
+            player.sendMessage(ChatColor.GRAY + "Use: " + ChatColor.WHITE + "/betterclear " + ChatColor.GRAY + "to get " + ChatColor.WHITE + "help" + ChatColor.GRAY + ".");
             return false;
         }
-        if (args[2] == null || !section.contains(args[2])){
-            player.sendMessage(ChatColor.GRAY + "Please provide a valid place to clear.");
+        if (args.length < 3 || (args[2] == null || !section.contains(args[2]))){
+            player.sendMessage(ChatColor.GRAY + "Please provide a valid" + ChatColor.WHITE + " section" + ChatColor.GRAY + " to clear.");
+            player.sendMessage(ChatColor.GRAY + "Use: " + ChatColor.WHITE + "/betterclear " + ChatColor.GRAY + "to get " + ChatColor.WHITE + "help" + ChatColor.GRAY + ".");
             return false;
         }
 
--- src/main/java/com/lukaos187/betterclear/util/ClearManager.java
+++ src/main/java/com/lukaos187/betterclear/util/ClearManager.java
@@ -1,6 +1,7 @@
 
 package com.lukaos187.betterclear.util;
 
+import jdk.nashorn.internal.ir.SplitReturn;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -10,7 +11,9 @@ import org.bukkit.inventory.ItemStack;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
+/**Represents a specific section of the inventory e.g. the hotbar.*/
 enum SLOTS{
     HOTBAR(Arrays.asList(0,1,2,3,4,5,6,7,8)),
     INVENTORY(Arrays.asList(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35)),
@@ -24,11 +27,15 @@ enum SLOTS{
         this.slots = slots;
     }
 
+    /**Returns a List<Integer> slots of the corresponding inventory slots of this section.*/
     public List<Integer> getSlots() {
         return slots;
     }
 }
-
+/**A class to manage the clearing conditions of the inventory. Initialized with the player as the sender of the command
+ * and the String[] args as the command arguments. The arguments have to be correct otherwise this fails to do his job.
+ * In this plugin the conditions for the arguments have been tested in the Clear class before initializing the
+ * ClearManager.*/
 public class ClearManager {
 
     private final Player sender;
@@ -60,62 +67,47 @@ public class ClearManager {
         slotsToClear = getSlots();
 
         if (typeToClear.equalsIgnoreCase("blocks")){
-            clearBlocks();
+            clearInventory(item -> item.getType().isBlock());
         }
         else if (typeToClear.equalsIgnoreCase("armor")) {
-            clearArmor();
+            clearInventory(item -> isArmor(item.getType()));
         }
         else if (typeToClear.equalsIgnoreCase("tools")){
-            clearTools();
+            clearInventory(item -> isTool(item.getType()));
         }
         else if (typeToClear.equalsIgnoreCase("items")){
-            clearItems();
+            clearInventory(item -> (!isTool(item.getType()) && !isArmor(item.getType()) && !item.getType().isBlock()));
         }
         else {
             targets.forEach(target -> target.getInventory().clear());
         }
 
         if (targets.size() > 1){
-            sender.sendMessage(ChatColor.GRAY + placeToClear.toUpperCase() + " of " + ChatColor.WHITE + "everyone " + ChatColor.GRAY + "has been cleared of " + ChatColor.WHITE + typeToClear + ChatColor.GRAY + ".");
+            sender.sendMessage(ChatColor.WHITE + capitalizeFirstLetter(placeToClear.toUpperCase()) + ChatColor.GRAY + " of " + ChatColor.WHITE + "everyone " + ChatColor.GRAY + "has been cleared of " + ChatColor.WHITE + typeToClear + ChatColor.GRAY + ".");
         }else {
-            sender.sendMessage(ChatColor.GRAY + placeToClear.toUpperCase() + " of " + ChatColor.WHITE + targets.get(0).getName() + ChatColor.GRAY + " has been cleared of " + ChatColor.WHITE + typeToClear + ChatColor.GRAY + ".");
+            sender.sendMessage(ChatColor.WHITE + capitalizeFirstLetter(placeToClear.toUpperCase()) + ChatColor.GRAY + " of " + ChatColor.WHITE + targets.get(0).getName() + ChatColor.GRAY + " has been cleared of " + ChatColor.WHITE + typeToClear + ChatColor.GRAY + ".");
         }
     }
 
-
-    /**Clears all the items that are not armour, blocks or tools from the given slots.*/
-    private void clearItems() {
-
-        targets.forEach(target -> {
-
-            slotsToClear.forEach(slot -> {
-
-                ItemStack item = target.getInventory().getItem(slot);
-                if (item != null && !isTool(item.getType()) && !isArmor(item.getType()) && !item.getType().isBlock()){
-
-                    target.getInventory().clear(slot);
-
-                }
-            });
-
-        });
+    /**Capitalizes the first letter of the given String and returns the formatted String.*/
+    private String capitalizeFirstLetter(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
-    /**Clears all the tools from the given slots.*/
-    private void clearTools() {
+    /**Clears the inventory of the player. The given Predicate acts as the condition under which the items get cleared.
+     *  e.g. item -> isArmor(item) //all the items that are armor get cleared.*/
+    private void clearInventory(Predicate<ItemStack> test){
 
-        targets.forEach(target -> {
+        targets.forEach(target -> slotsToClear.forEach(slot -> {
 
-            slotsToClear.forEach(slot -> {
-
-                ItemStack item = target.getInventory().getItem(slot);
-                if (item != null && isTool(item.getType())) {
-
-                    target.getInventory().clear(slot);
-
-                }
-            });
-        });
+            ItemStack item = target.getInventory().getItem(slot);
+            if (item != null && test.test(item)){
+                target.getInventory().clear(slot);
+            }
+        }));
     }
 
     /**Tests if the Material is a tool*/
@@ -127,49 +119,12 @@ public class ClearManager {
                 material.name().endsWith("_SWORD");
     }
 
-    /**Clears all the armour from the given slots.*/
-    private void clearArmor() {
-
-        targets.forEach(target -> {
-
-            slotsToClear.forEach(slot -> {
-
-                ItemStack item = target.getInventory().getItem(slot);
-                if (item != null && isArmor(item.getType())){
-
-                    target.getInventory().clear(slot);
-
-                }
-            });
-
-        });
-    }
-
     /**Tests if the Material is an armour-piece*/
     private boolean isArmor(Material material) {
         return material.name().endsWith("_HELMET") ||
                 material.name().endsWith("_CHESTPLATE") ||
                 material.name().endsWith("_LEGGINGS") ||
                 material.name().endsWith("_BOOTS");
-    }
-
-    /**Clears all the blocks from the given slots.*/
-    private void clearBlocks() {
-
-        targets.forEach(target -> {
-
-            slotsToClear.forEach(slot -> {
-
-                ItemStack item = target.getInventory().getItem(slot);
-                if (item != null && item.getType().isBlock()){
-
-                    target.getInventory().clear(slot);
-
-                }
-            });
-
-        });
-
     }
 
     /**Returns the slots which have to be cleared.*/
